1db02a65a5a4ea23b63b754fb063a0ca01b9c7d1
deirdre <deirdre.loyall@gmail.com>
2023-05-12 17:48:13 -0400

    languages circle now seems to be displaying icons in circle . . . just not in the right place

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ let slides = [
   '.contact'
 ]
 
+let languages = ['images/js.png',
+                 'images/typescript.png',
+                 'images/python.png'];
+
+
 let updatePage = () => {
   $(slides[counter]).toggleClass('active');
   counter += 1;
@@ -19,11 +24,23 @@ let updatePage = () => {
   // console.log(`counter is ${counter}`);
 }
 
+let langTan = Math.tan(Math.PI/languages.length);
 
 $(document).ready(function() {
 
   $('.button').click(() => {
     updatePage();
   })
+
+  let $techList = $('.tech-list');
+  let $langNode = $(`<div class="node lang" style="
+                          --m: ${languages.length};
+                          --tan: ${langTan.toFixed(2)};">Languages</div>`);
+  $techList.append($langNode);
+  for (let i = 0; i < languages.length; i++) {
+    const $language = $(`<img class="icon" style="--i: ${i};" src=${languages[i]} />`)
+    $langNode.append($language);
+  }
+
 
 })
